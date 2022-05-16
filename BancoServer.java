3597e95a51f506818b429896480efc948a07893c
Thiago Defini Scherer <Thiago.Scherer@edu.pucrs.br>
2022-05-16 19:59:41 -0300

    First Commit

---
+++ BancoServer.java
@@ -0,0 +1,27 @@
+import java.rmi.Naming;
+import java.rmi.Remote;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class BancoServer {
+    public BancoServer(){
+        try {
+            //Definicao do ip onde o servico ira funcionar
+            System.setProperty("java.rmi.server.hostname", "localhost");
+            //Registro do servico em uma porta
+            LocateRegistry.createRegistry(1099);
+            //Cria o objeto que implementa os metodos que serao servidos
+            Banco b = new BancoImp();
+            //Coloca na porta registrada o servico da calculadora
+            Naming.bind("CalcService", (Remote) b);
+            System.out.println("Conexao estabelecida.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new BancoServer();
+    }
+}
