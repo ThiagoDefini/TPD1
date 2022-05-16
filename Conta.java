65e549d2beef8c8e4220b964df044d969c487a55
Thiago Defini Scherer <Thiago.Scherer@edu.pucrs.br>
2022-05-16 20:10:32 -0300

    Adicionando Conta

---
+++ Conta.java
@@ -0,0 +1,36 @@
+public class Conta {
+    private String nome;
+    private int cpf;
+    private Double saldo;
+
+    public Conta(String nome, int cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.saldo = 0.0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+    
+}
